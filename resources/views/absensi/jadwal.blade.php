d7ab1e6223b638fd0e433a9f5a9cbc2a9997a530
adimertap <adimertap@gmail.com>
2024-07-04 00:03:17 +0700

    Fix Issue Jadwal User Null and Shift Null

--- resources/views/absensi/jadwal.blade.php
+++ resources/views/absensi/jadwal.blade.php
@@ -13,7 +13,6 @@
         font-weight: bold;
         /* Bold text */
     }
-
 </style>
 <main>
     <div class="d-flex justify-content-between mt-3">
@@ -28,9 +27,12 @@
 
     <div class="card overflow-hidden p-4">
         <ul class="nav nav-tabs" id="myTab" role="tablist">
-            <li class="nav-item"><a class="nav-link active" id="home-tab" data-bs-toggle="tab" href="#tab-home" role="tab" aria-controls="tab-home" aria-selected="true">Jadwal Kerja</a></li>
-            <li class="nav-item"><a class="nav-link" id="profile-tab" data-bs-toggle="tab" href="#tab-profile" role="tab" aria-controls="tab-profile" aria-selected="false">Request Tukar Jadwal</a></li>
-            {{-- <li class="nav-item"><a class="nav-link" id="contact-tab" data-bs-toggle="tab" href="#tab-contact" role="tab" aria-controls="tab-contact" aria-selected="false">Contact</a></li> --}}
+            <li class="nav-item"><a class="nav-link active" id="home-tab" data-bs-toggle="tab" href="#tab-home"
+                    role="tab" aria-controls="tab-home" aria-selected="true">Jadwal Kerja</a></li>
+            <li class="nav-item"><a class="nav-link" id="profile-tab" data-bs-toggle="tab" href="#tab-profile"
+                    role="tab" aria-controls="tab-profile" aria-selected="false">Request Tukar Jadwal</a></li>
+            {{-- <li class="nav-item"><a class="nav-link" id="contact-tab" data-bs-toggle="tab" href="#tab-contact"
+                    role="tab" aria-controls="tab-contact" aria-selected="false">Contact</a></li> --}}
         </ul>
         <div class="tab-content" id="myTabContent">
             <div class="tab-pane fade show active" id="tab-home" role="tabpanel" aria-labelledby="home-tab">
@@ -41,7 +43,8 @@
             <div class="tab-pane fade" id="tab-profile" role="tabpanel" aria-labelledby="profile-tab">
                 <div class="card-body">
                     <i class="mb-3">Data Pegawai yang ingin tukar jadwal</i>
-                    <div class="mt-3" id="tableTukar" data-list='{"valueNames":["no","nama_currency","country"],"page":20,"pagination":true}'>
+                    <div class="mt-3" id="tableTukar"
+                        data-list='{"valueNames":["no","nama_currency","country"],"page":20,"pagination":true}'>
                         <div class="table-responsive scrollbar">
                             <table class="table table-bordered table-striped fs--1 mb-0" id="datatable">
                                 <thead class="bg-200 text-900">
@@ -58,12 +61,15 @@
                                     @foreach ($tukar as $item)
                                     <tr role="row" class="odd">
                                         <th scope="row" class="no">{{ $loop->iteration }}.</th>
-                                        <td class="text-center">{{ Carbon\Carbon::parse($item->tanggal)->format('d F Y') }}</td>
+                                        <td class="text-center">{{ Carbon\Carbon::parse($item->tanggal)->format('d F Y')
+                                            }}</td>
                                         <td class="text-center">{{ $item->Shift->shift_name }}</td>
                                         <td class="text-center">{{ $item->User->name }}</td>
                                         <td>{{ $item->keterangan }}</td>
                                         <td class="text-center">
-                                            <button class="btn btn-sm btn-primary" style="font-size: 12px!important" onclick="updateTukar('{{ $item->tanggal }}','{{ $item->jadwal_id }}','{{ $item->User->id }}','{{ $item->shift_id }}')" type="button">Update</button>
+                                            <button class="btn btn-sm btn-primary" style="font-size: 12px!important"
+                                                onclick="updateTukar('{{ $item->tanggal }}','{{ $item->jadwal_id }}','{{ $item->User->id }}','{{ $item->shift_id }}')"
+                                                type="button">Update</button>
                                         </td>
 
                                     </tr>
@@ -84,7 +90,8 @@
     <div class="modal-dialog modal-dialog-centered" role="document" style="max-width: 600px">
         <div class="modal-content position-relative">
             <div class="position-absolute top-0 end-0 mt-2 me-2 z-index-1">
-                <button class="btn-close btn btn-sm btn-circle d-flex flex-center transition-base" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button class="btn-close btn btn-sm btn-circle d-flex flex-center transition-base"
+                    data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <form action="{{ route('jadwal.store') }}" method="POST" id="jadwalForm" enctype="multipart/form-data">
                 @csrf
@@ -94,17 +101,22 @@
                         <p class="text-word-break fs--1">Lengkapi Form berikut ini</p>
                         <div class="row mt-4 mb-2">
                             <div class="col-md-6 mb-3">
-                                <label class="form-label" for="start_date">Start Date</label><span class="mr-4 mb-3" style="color: red">*</span>
-                                <input class="form-control @error('start_date') is-invalid @enderror" name="start_date" type="date" placeholder="Input Start Date" value="{{ old('start_date') }}" />
+                                <label class="form-label" for="start_date">Start Date</label><span class="mr-4 mb-3"
+                                    style="color: red">*</span>
+                                <input class="form-control @error('start_date') is-invalid @enderror" name="start_date"
+                                    type="date" placeholder="Input Start Date" value="{{ old('start_date') }}" />
                             </div>
                             <div class="col-md-6 mb-3">
                                 <label class="form-label" for="end_date">End Date</label>
-                                <input class="form-control @error('end_date') is-invalid @enderror" name="end_date" type="date" placeholder="Input End Date" value="{{ old('end_date') }}" />
+                                <input class="form-control @error('end_date') is-invalid @enderror" name="end_date"
+                                    type="date" placeholder="Input End Date" value="{{ old('end_date') }}" />
                             </div>
                         </div>
                         <div class="col-md-12 mb-3">
-                            <label class="form-label" for="pegawai">Pegawai</label><span class="mr-4 mb-3" style="color: red">*</span>
-                            <select type="text" id="pegawai" name="pegawai" class="form-select" value="{{ old('pegawai') }}">
+                            <label class="form-label" for="pegawai">Pegawai</label><span class="mr-4 mb-3"
+                                style="color: red">*</span>
+                            <select type="text" id="pegawai" name="pegawai" class="form-select"
+                                value="{{ old('pegawai') }}">
                                 <option value="">Pilih Pegawai</option>
                                 @foreach ($user as $item)
                                 <option value="{{ $item->id }}">
@@ -118,7 +130,8 @@
                             <span class="mr-4 mb-3" style="color: red">*</span>
                             @foreach ($shift as $item)
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="shift" value="{{ $item->shift_id }}" id="shift_{{ $item->shift_id }}">
+                                <input class="form-check-input" type="radio" name="shift" value="{{ $item->shift_id }}"
+                                    id="shift_{{ $item->shift_id }}">
                                 <label class="form-check-label" for="shift_{{ $item->shift_id }}">
                                     {{ $item->shift_name }}, In: {{ $item->shift_in }}, Out: {{ $item->shift_out }}
                                 </label>
@@ -128,30 +141,33 @@
                         {{-- <div class="row mt-1 mb-2">
                             <div class="col-md-12 mb-3">
                                 <label class="form-label" for="keterangan">Keterangan</label>
-                                <textarea class="form-control" name="keterangan" type="text" placeholder="Input keterangan" rows="2" value="{{ old('keterangan') }}"></textarea>
+                                <textarea class="form-control" name="keterangan" type="text"
+                                    placeholder="Input keterangan" rows="2" value="{{ old('keterangan') }}"></textarea>
+                            </div>
+                        </div> --}}
+                        <div class="mb-4">
+                            <span class="mb-4" style="color: red">*</span> <span class="fs--1">Wajib diisi</span>
+                        </div>
                     </div>
-                </div> --}}
-                <div class="mb-4">
-                    <span class="mb-4" style="color: red">*</span> <span class="fs--1">Wajib diisi</span>
                 </div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-bs-dismiss="modal">Close</button>
+                    <button class="btn btn-primary" id="btnModal" type="submit">Save </button>
+                </div>
+            </form>
         </div>
     </div>
-    <div class="modal-footer">
-        <button class="btn btn-secondary" type="button" data-bs-dismiss="modal">Close</button>
-        <button class="btn btn-primary" id="btnModal" type="submit">Save </button>
-    </div>
-    </form>
-</div>
-</div>
 </div>
 
 <div class="modal fade" id="modal-edit" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document" style="max-width: 600px">
         <div class="modal-content position-relative">
             <div class="position-absolute top-0 end-0 mt-2 me-2 z-index-1">
-                <button class="btn-close btn btn-sm btn-circle d-flex flex-center transition-base" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button class="btn-close btn btn-sm btn-circle d-flex flex-center transition-base"
+                    data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <form action="{{ route('jadwal.update', 1) }}" method="POST" id="jadwalEditForm" enctype="multipart/form-data">
+            <form action="{{ route('jadwal.update', 1) }}" method="POST" id="jadwalEditForm"
+                enctype="multipart/form-data">
                 @method('PUT')
                 @csrf
                 <input type="hidden" id="jadwalIdEdit" name="jadwalIdEdit">
@@ -161,12 +177,15 @@
                         <div class="row mt-4 mb-2">
                             <div class="col-md-12 mb-3">
                                 <label class="form-label" for="start_date">Date</label>
-                                <input class="form-control" name="start_date" id="startDateEdit" type="date" placeholder="Input Start Date" value="{{ old('start_date') }}" readonly />
+                                <input class="form-control" name="start_date" id="startDateEdit" type="date"
+                                    placeholder="Input Start Date" value="{{ old('start_date') }}" readonly />
                             </div>
                         </div>
                         <div class="col-md-12 mb-3">
-                            <label class="form-label" for="pegawai">Pegawai</label><span class="mr-4 mb-3" style="color: red">*</span>
-                            <select type="text" id="pegawaiEdit" name="pegawai" class="form-select" value="{{ old('pegawai') }}">
+                            <label class="form-label" for="pegawai">Pegawai</label><span class="mr-4 mb-3"
+                                style="color: red">*</span>
+                            <select type="text" id="pegawaiEdit" name="pegawai" class="form-select"
+                                value="{{ old('pegawai') }}">
                                 <option value="">Pilih Pegawai</option>
                                 @foreach ($user as $item)
                                 <option value="{{ $item->id }}">
@@ -180,7 +199,8 @@
                             <span class="mr-4 mb-3" style="color: red">*</span>
                             @foreach ($shift as $item)
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="shiftEdit" value="{{ $item->shift_id }}" id="edit_shift_{{ $item->shift_id }}">
+                                <input class="form-check-input" type="radio" name="shiftEdit"
+                                    value="{{ $item->shift_id }}" id="edit_shift_{{ $item->shift_id }}">
                                 <label class="form-check-label" for="shift_{{ $item->shift_id }}">
                                     {{ $item->shift_name }}, In: {{ $item->shift_in }}, Out: {{ $item->shift_out }}
                                 </label>
@@ -190,24 +210,25 @@
                         {{-- <div class="row mt-1 mb-2">
                             <div class="col-md-12 mb-3">
                                 <label class="form-label" for="keteranganEdit">Keterangan</label>
-                                <textarea class="form-control" id="keteranganEdit" name="keterangan" type="text" placeholder="Input keterangan" rows="2" value="{{ old('keterangan') }}" ></textarea>
+                                <textarea class="form-control" id="keteranganEdit" name="keterangan" type="text"
+                                    placeholder="Input keterangan" rows="2" value="{{ old('keterangan') }}"></textarea>
+                            </div>
+                        </div> --}}
+                        <div class="mb-4">
+                            <span class="mb-4" style="color: red">*</span> <span class="fs--1">Wajib diisi</span>
+                        </div>
                     </div>
-                </div> --}}
-                <div class="mb-4">
-                    <span class="mb-4" style="color: red">*</span> <span class="fs--1">Wajib diisi</span>
                 </div>
+                <div class="modal-footer d-flex justify-content-between">
+                    <button class="btn btn-danger" id="btnDelete" onclick="deleteJadwal()" type="button">Delete</button>
+                    <div>
+                        <button class="btn btn-secondary" type="button" data-bs-dismiss="modal">Close</button>
+                        <button class="btn btn-primary" id="btnEdit" type="submit">Update</button>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
-    <div class="modal-footer d-flex justify-content-between">
-        <button class="btn btn-danger" id="btnDelete" onclick="deleteJadwal()" type="button">Delete</button>
-        <div>
-            <button class="btn btn-secondary" type="button" data-bs-dismiss="modal">Close</button>
-            <button class="btn btn-primary" id="btnEdit" type="submit">Update</button>
-        </div>
-    </div>
-    </form>
-</div>
-</div>
 </div>
 
 
@@ -318,17 +339,19 @@
                         , success: function(data) {
                             var events = [];
                             data.forEach(function(jadwal) {
-                                var eventColor;
-                                events.push({
-                                    id: jadwal.jadwal_id, // Assuming you have an ID for each event
-                                    title: `${jadwal.user.name} - ${jadwal.shift.shift_name}`
-                                    , start: jadwal.tanggal
-                                    , end: jadwal.tanggal_akhir, // Assuming your date fields are single day events
-                                    extendedProps: {
-                                        shift_in: jadwal.shift.shift_in
-                                        , shift_out: jadwal.shift.shift_out
-                                    }
-                                });
+                                if (jadwal.user && jadwal.user.name && jadwal.shift && jadwal.shift.shift_name) {
+                                    var eventColor;
+                                    events.push({
+                                        id: jadwal.jadwal_id, // Assuming you have an ID for each event
+                                        title: `${jadwal.user.name} - ${jadwal.shift.shift_name}`
+                                        , start: jadwal.tanggal
+                                        , end: jadwal.tanggal_akhir, // Assuming your date fields are single day events
+                                        extendedProps: {
+                                            shift_in: jadwal.shift.shift_in
+                                            , shift_out: jadwal.shift.shift_out
+                                        }
+                                    });
+                                }
                             });
                             successCallback(events);
                         }
